50c8032200facf12e8241042efdb40171229f868
Akash Verma <43105736+a4aakash@users.noreply.github.com>
2019-05-02 00:10:19 +0530

    Update image_recognize.py

--- image_recognize.py
+++ image_recognize.py
@@ -1,9 +1,9 @@
 import face_recognition as fr
 import cv2
 
-img= fr.load_image_file("pp1.jpg")
+img= fr.load_image_file("Put Your image with extension")
 img_encode = fr.face_encodings(img)[0]
-real = fr.load_image_file("pp1.jpg")
+real = fr.load_image_file("Put Image For check with extension")
 real_encode = fr.face_encodings(real)[0]
 result = fr.compare_faces([img_encode], real_encode)
 print(result)
