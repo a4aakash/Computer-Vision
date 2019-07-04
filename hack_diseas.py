af6e04318991da182fe34bc24d9c0e832299d5bf
Akash Verma <43105736+a4aakash@users.noreply.github.com>
2019-07-04 14:04:35 +0530

    Update hack_diseas.py
    A solution to a problem on HAcker_EArth.

--- hack_diseas.py
+++ hack_diseas.py
@@ -1,9 +1,14 @@
+# IMportIng The LIbrary
 import numpy as np
 import pandas as pd
 from sklearn.naive_bayes import GaussianNB
 
+# Processing and ReaDing THE DAta sets
+
 tt = pd.read_csv(r"C:\Users\HP\Desktop\train.csv")
 ts =pd.read_csv(r"C:\Users\HP\Desktop\test.csv")
+
+# MAke It CompatiAble
 
 tt['gender'].replace(['M','F'],[0,1],inplace=True)
 ts['gender'].replace(['M','F'],[0,1],inplace=True)
@@ -11,13 +16,15 @@ ts['gender'].replace(['M','F'],[0,1],inplace=True)
 train_1 = tt.iloc[:,1:4]
 test_1 = ts.iloc[:,1:4]
 
-
-
 target = tt.detected
+
+# Choose and Create THE object of ML-MODEL
 
 obj = GaussianNB()
 obj = obj.fit(train_1,target)
 pred = obj.predict(test_1)
+
+# Now makeing a .CSV file for USer
 
 submit = pd.DataFrame({'row_id':ts['row_id'],'detected':pred})
 submit = submit[['row_id','detected']]
